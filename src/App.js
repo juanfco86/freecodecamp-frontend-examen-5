515988e3458c1d55e2243d1175a1cebaf1d2227e
juanfco86 <juanfco86@gmail.com>
2023-05-26 21:47:47 +0200

    solution: step 2

--- src/App.js
+++ src/App.js
@@ -1,51 +1,53 @@
 import { useState } from 'react';
 import './App.css';
 import Footer from './Components/Footer/Footer';
+import { useEffect } from 'react';
 
 function App() {
   const [breakLabel, setBreakLabel] = useState(5);
   const [sessionLabel, setSessionLabel] = useState(25);
+  const [result, setResult] = useState(`${sessionLabel}:00`)
   const [timerStatusSession, setTimerStatusSession] = useState(false)
   const [timerStatusBreak, setTimerStatusBreak] = useState(false)
+
+  useEffect(() => {
+    setResult(`${sessionLabel}:00`)
+  }, [sessionLabel]);
   
-  const secondsSession = 1000
-  const minutesSession = secondsSession * 60
-  const hoursSession = minutesSession * 60
-  
-  const updateCountSession = () => {
-    const today = new Date();
-    const seconds = Math.floor((today % minutesSession) / secondsSession)
-    const minutes = Math.floor((today % hoursSession) / minutesSession)
+  const updateCountSession = (minutes, seconds) => {
     if (seconds < 10) {
       return `${minutes}:0${seconds}`
+    } else if (minutes < 10) {
+      return `0${minutes}:${seconds}`;
+    } else if (minutes < 10 && seconds < 10) {
+      return `0${minutes}:0${seconds}`;
     } else {
       return `${minutes}:${seconds}`;
     }
   }
-  console.log(updateCountSession());
-  setInterval(updateCountSession, 1000)
   
   const timer = () => {
-    const secondsSessionTimer = sessionLabel * 60 * 1000
-    const secondsBreakTimer = breakLabel * 60 * 1000
+    let seconds = 0;
+    let minutes = sessionLabel
+    setTimerStatusSession(true)
 
-    if (timerStatusSession) {
-      setTimerStatusSession(false)
-    } else {
-      setTimerStatusSession(true)
-    }
+    const countdown = setInterval(() => {
+      seconds--;
+      if (seconds < 0) {
+        seconds = 59;
+        minutes--;
+      }
+      const duration = updateCountSession(minutes, seconds)
 
-    setTimeout(() => {
-      console.log('CULAZO');
-      setTimerStatusSession(false)
-      setTimerStatusBreak(true)
+      if (minutes === 0 && seconds === 0) {
+        clearInterval(countdown);
+        console.log('TIEMPO FINALIZADO');
+      }
+      setResult(duration);
 
-      setTimeout(() => {
-        console.log('TETAZAS');
-        setTimerStatusBreak(false)
-      }, secondsBreakTimer)
+      return result;
+    }, 1000)
 
-    }, secondsSessionTimer)
   }
 
 
@@ -94,21 +96,21 @@ function App() {
             <div id="break-label">
                 <h6 className="h6--style">Break Length</h6>
                 <button id="break-decrement" onClick={ breakDecrement }>
-                  <i class="bi bi-dash-circle"></i>
+                  <i className="bi bi-dash-circle"></i>
                 </button>
                 <p id="break-length">{ breakLabel }</p>
                 <button id="break-increment" onClick={ breakIncrement }>
-                  <i class="bi bi-plus-circle"></i>
+                  <i className="bi bi-plus-circle"></i>
                 </button>
             </div>
             <div id="session-label">
                 <h6 className="h6--style">Session Length</h6>
                 <button id="session-decrement" onClick={ sessionDecrement }>
-                  <i class="bi bi-dash-circle"></i>
+                  <i className="bi bi-dash-circle"></i>
                 </button>
                 <p id="session-length">{ sessionLabel }</p>
                 <button id="session-increment" onClick={ sessionIncrement }>
-                  <i class="bi bi-plus-circle"></i>
+                  <i className="bi bi-plus-circle"></i>
                 </button>
             </div>
         </div>
@@ -116,21 +118,22 @@ function App() {
           <h6 id="timer-label">Session</h6>
             <p className='time-value'>
               {
-                !timerStatusSession ? `${sessionLabel}:00` : 'PRUEBA'
+                result
               }
             </p>
         </div>
         <div className='container--icons'>
           <button id="start_stop" className="button--style" onClick={ timer }>
             {
-              !timerStatusSession ? <i class="bi bi-play-circle-fill icons--style"></i> : <i className="bi bi-pause-circle-fill icons--style"></i>
+              !timerStatusSession ? <i className="bi bi-play-circle-fill icons--style"></i> : <i className="bi bi-pause-circle-fill icons--style"></i>
             }
           </button>
           <button id="reset" className="button--style" onClick={ resetAll }>
-            <i class="bi bi-x-circle-fill icons--style"></i>
+            <i className="bi bi-x-circle-fill icons--style"></i>
           </button>
           
         </div>
+        
         <Footer />
 
         {/* AUDIO CON UN SONIDO BEEP PARA FINALIZAR? */}
